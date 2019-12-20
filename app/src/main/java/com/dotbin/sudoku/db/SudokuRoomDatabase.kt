32b181148b90c700c387c92bea05317995a50314
dotbin <846400461@qq.com>
2019-12-20 17:30:25 +0800

    add database type converters

--- app/src/main/java/com/dotbin/sudoku/db/SudokuRoomDatabase.kt
+++ app/src/main/java/com/dotbin/sudoku/db/SudokuRoomDatabase.kt
@@ -1,11 +1,10 @@
 package com.dotbin.sudoku.db
 
 import android.content.Context
-import androidx.room.Database
-import androidx.room.Room
-import androidx.room.RoomDatabase
+import androidx.room.*
 
 @Database(entities = [SudokuGame::class], version = 1)
+@TypeConverters(SudokuConverters::class)
 abstract class SudokuRoomDatabase : RoomDatabase() {
     abstract fun SudokuDao(): SudokuDao
 
